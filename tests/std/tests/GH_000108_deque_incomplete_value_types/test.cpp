b4d440a00c56f5c5b1f9b0137fae87f3b387b98e
A. Jiang <de34@live.cn>
2024-02-14 17:49:05 +0800

    Make `deque` accept incomplete element types.

---
+++ tests/std/tests/GH_000108_deque_incomplete_value_types/test.cpp
@@ -0,0 +1,46 @@
+// Copyright (c) Microsoft Corporation.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <cassert>
+#include <deque>
+
+using namespace std;
+
+void test_gh_000108_push_pop_size() {
+    struct recur {
+        deque<recur> d;
+    };
+
+    recur x;
+
+    for (int n = 0; n < 1000; ++n) {
+        x.d.push_back(recur{});
+
+        if (x.d.size() < 26) {
+            continue;
+        }
+
+        x.d.pop_front();
+    }
+}
+
+void test_gh_000108_push_pop_size_with_payload() {
+    struct recur_payload {
+        int n;
+        deque<recur_payload> d;
+    };
+
+    recur_payload x{};
+
+    x.d.push_back({10, {}});
+    x.d.push_back({20, {}});
+    x.d.push_front({30, {}});
+    x.d.push_front({40, {}});
+
+    assert(x.d.size() == 4 && x.d[0].n == 40 && x.d[1].n == 30 && x.d[2].n == 10 && x.d[3].n == 20);
+}
+
+int main() {
+    test_gh_000108_push_pop_size();
+    test_gh_000108_push_pop_size_with_payload();
+}
