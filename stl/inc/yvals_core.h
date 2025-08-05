0597c14ecb276870263619ce687f6eb5cf860dfe
A. Jiang <de34@live.cn>
2025-08-05 15:08:20 +0800

    Implement P3168R2 `std::optional` Range Support
    Along with P3913R1 Optimize For `std::optional` In Range Adaptors,
    except that the part for `views::as_const` and `optional<T&>` is not
    implemented.

--- stl/inc/yvals_core.h
+++ stl/inc/yvals_core.h
@@ -413,6 +413,7 @@
 // _HAS_CXX26 controls:
 // P1383R2 More constexpr For <cmath> And <complex>
 //     (partial implementation)
+// P3168R2 std::optional Range Support
 
 // Parallel Algorithms Notes
 // C++ allows an implementation to implement parallel algorithms as calls to the serial algorithms.
@@ -1817,6 +1818,11 @@ _EMIT_STL_ERROR(STL1004, "C++98 unexpected() is incompatible with C++23 unexpect
 #define __cpp_lib_tuple_like                  202207L
 #define __cpp_lib_unreachable                 202202L
 #endif // _HAS_CXX23
+
+// C++26
+#if _HAS_CXX26
+#define __cpp_lib_optional_range_support 202406L
+#endif // _HAS_CXX26
 
 // macros with language mode sensitivity
 #if _HAS_CXX20
